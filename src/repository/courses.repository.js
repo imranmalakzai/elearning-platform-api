6ad7c24ad6218b6f4738239b14333bce90cf02aa
unknown <imranmalakzai>
2025-12-21 10:28:12 +0430

    Gell courses of on instructory from db

--- src/repository/courses.repository.js
+++ src/repository/courses.repository.js
@@ -14,3 +14,11 @@ export const courses = async () => {
   const [rows] = await pool.query("SELECT * FROM courses");
   return rows;
 };
+
+//** GET all courses of one instructore */
+export const instructorCourses = async (instructor_id) => {
+  const [rows] = await pool.query(
+    "SELECT * FROM courses WHERE instructor_id = ?",
+    [instructor_id]
+  );
+};
