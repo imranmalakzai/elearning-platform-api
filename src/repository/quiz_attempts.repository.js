50367d143e52f76d3953640e59f6d0c14abffd94
= <=>
2025-12-21 21:16:53 +0430

    update Quize attempts by a student

--- src/repository/quiz_attempts.repository.js
+++ src/repository/quiz_attempts.repository.js
@@ -1,9 +1,19 @@
 import pool from "../config/db.config.js";
 
-//**Add Quize Attempts */
-const createQuizeAttempts = async (data) => {
+//**Add user score in a test  Attempts */
+export const createQuizeAttempts = async (data) => {
   const result = await pool.query(
     "INSERT INTO quiz_attempts (user_id,quiz_id,score) VALUES (?,?,?)",
     [data.user_id, data.quiz_id, data.score]
   );
+  return result.affectedRows;
+};
+
+//**Update Quize Attempt of a user*/
+export const updateQuizeAttempts = async (data, id) => {
+  const result = await pool.query(
+    "UPDATE quize_attempts SET user_id = ? , quiz_id = ? WHERE id = ? ",
+    [data.user_id, data.quiz_id, id]
+  );
+  return result.affectedRows;
 };
