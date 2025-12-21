93e5f2cb96123d69fbd7c44737286259b7883ef1
unknown <imranmalakzai>
2025-12-21 07:15:53 +0430

    get user by email

--- src/repository/users.repository.js
+++ src/repository/users.repository.js
@@ -5,3 +5,11 @@ export const getAllUsers = async () => {
   const [rows] = await pool.query("SELECT * FROM users");
   return rows;
 };
+
+/**GET A USER BY Email */
+export const getUserByEmail = async (email) => {
+  const [rows] = await pool.query("SELECT * FROM users WHERE email = ?", [
+    email,
+  ]);
+  return rows[0];
+};
