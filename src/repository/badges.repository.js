3b9c7f862e2416476f9e91e6d177306d4e1feb1a
= <=>
2025-12-21 17:16:20 +0430

    update Badge

--- src/repository/badges.repository.js
+++ src/repository/badges.repository.js
@@ -6,4 +6,14 @@ export const createBadge = async (data) => {
     "INSERT INTO badges (name,description,points_required)",
     [data.name, data.description, data.points_required]
   );
+  return result.affectedRows;
+};
+
+//**update A badge */
+export const updateBadge = async (data, id) => {
+  const result = await pool.query(
+    "UPDATE badges SET name = ?, description = ?, points_required = ? WHERE id = ?",
+    [data.name, data.description, data.points_required, id]
+  );
+  return result.affectedRows;
 };
