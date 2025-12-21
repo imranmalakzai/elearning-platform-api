abf70d6828b6dc7de350f511ba78c819f14959d9
= <=>
2025-12-21 12:43:10 +0430

    Add update lessons sql logic

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,4 +1,4 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import { HOST, DB_NAME, DB_USER, DB_PASSWORD } from "./env.config.js";
 
 const pool = mysql.createPool({
--- src/repository/lessons.repository.js
+++ src/repository/lessons.repository.js
@@ -14,3 +14,19 @@ export const createLessons = async (data) => {
   );
   return result.affectedRows;
 };
+
+//**update lesson in a course */
+export const updateLessons = async (data, id) => {
+  const result = await pool.query(
+    "UPDATE lessons SET course_id = ?, title = ?, content = ?, video_url = ? , lesson_order = ? WHERE id = ?",
+    [
+      data.course_id,
+      data.title,
+      data.content,
+      data.video_url,
+      data.lesson_order,
+      id,
+    ]
+  );
+  return result.affectedRows;
+};
