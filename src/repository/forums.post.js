567f04cf349da5791fca976a56e96d77b77318f8
= <=>
2025-12-22 06:28:34 +0430

    create a post in forum

--- src/repository/forums.post.js
+++ src/repository/forums.post.js
@@ -8,3 +8,12 @@ export const createPost = async (data) => {
   );
   return result.affectedRows;
 };
+
+//**update a forum post */
+export const updatePost = async (data, id) => {
+  const result = await pool.query(
+    "UPDATE forum_posts SET course_id = ?, user_id = ?, content = ? WHERE id = ?",
+    [data.course_id, data.user_id, data.content, id]
+  );
+  return result.affectedRows;
+};
