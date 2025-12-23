c4814bbc8ba43a17fdb2e84dd64f8f18e69dc206
imranmalakzai <afghanimran92@gmail.com>
2025-12-23 17:03:58 +0430

    create cancell Enrollment controller

--- src/controllers/enrollment.controller.js
+++ src/controllers/enrollment.controller.js
@@ -1,14 +1,40 @@
 import asyncHandler from "../utils/async_handler.js";
 import ApiError from "../utils/api_error.js";
 import { getCourseById } from "../repository/courses.repository.js";
-import { createEnrollment } from "../repository/enrollments.repository.js";
+import {
+  createEnrollment,
+  isEnrolled,
+  deleteEnrollment,
+} from "../repository/enrollments.repository.js";
 
-//**Enrolled to a course */
+//**Enrolled to a course (student only) */
 export const enrollToACourse = asyncHandler(async (req, res) => {
   const { course_id } = req.params;
   const course = await getCourseById(course_id);
   if (!course) throw new ApiError("course not exist", 404);
+
+  //check user enrollments
+  const record = await isEnrolled(course_id, req.user.id);
+  if (record) throw new ApiError("user already enrolled to this course", 409);
+
   const result = await createEnrollment(course_id, req.user.id);
   if (!result) throw new ApiError("Internal server error", 500);
   res.status(201).json({ message: "Enrolled successfully" });
+});
+
+//**Delete user enrollment (student only) */
+export const cancellEnrollment = asyncHandler(async (req, res) => {
+  const { course_id } = req.params;
+  const course = await getCourseById(course_id);
+  if (!course) throw new ApiError("course not exist", 404);
+
+  //check user enrollments
+  const record = await isEnrolled(course_id, req.user.id);
+  if (!record) throw new ApiError("Not enrolled", 404);
+
+  //delete record
+  const result = await deleteEnrollment(record.id);
+  if (!result) throw new ApiError("Internal server error", 500);
+
+  res.status(204).json({ message: "enrollment cancelled successfully" });
 });
