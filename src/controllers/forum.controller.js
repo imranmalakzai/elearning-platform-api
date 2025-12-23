31e6446b21d1cd1e99fa90e91a75f3a2a4cdf9af
imranmalakzai <afghanimran92@gmail.com>
2025-12-23 17:50:36 +0430

    update repo name

---
+++ src/controllers/forum.controller.js
@@ -0,0 +1,5 @@
+import asyncHandler from "../utils/async_handler.js";
+import ApiError from "../utils/api_error.js";
+
+//**create a post (Instroucter,enrolled student) only */
+const createPost = asyncHandler(async (req, res) => {});
