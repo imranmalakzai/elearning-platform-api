a193f7b4b7455d9bbf36f52613ff4a7a2db955e7
imranmalakzai <afghanimran92@gmail.com>
2025-12-24 21:14:59 +0430

    fix and rename the quizz spilling essue

--- src/controllers/questions.controller.js
+++ src/controllers/questions.controller.js
@@ -1,15 +1,18 @@
 import asyncHandler from "../utils/async_handler.js";
 import ApiError from "../utils/api_error.js";
 import { getCourseById } from "../repository/courses.repository.js";
-import { quizBelongToCouse } from "../repository/quizzes.repository.js";
-import { createQuestion as createQuestionRepo } from "../repository/questions.repository.js";
+import { quizBelongToCourse } from "../repository/quizzes.repository.js";
+import {
+  createQuestion as createQuestionRepo,
+  questionBelongsToCourse,
+  deleteQuestion,
+} from "../repository/questions.repository.js";
 
 //**create questions for a quizz of a course module */
 export const createQuestion = asyncHandler(async (req, rest) => {
-  const { course_id, quizz_id } = req.params;
+  const { course_id, quiz_id } = req.params;
   const { question, option_a, option_b, option_c, option_d, correct_option } =
     req.body;
-
   //check course exist
   const course = await getCourseById(course_id);
   if (!course) throw new ApiError("course not exit", 404);
@@ -19,7 +22,7 @@ export const createQuestion = asyncHandler(async (req, rest) => {
   if (!instructor) throw new ApiError("Access denied", 403);
 
   //check quizz exist and belong to course
-  const quizz = await quizBelongToCouse(course_id, quizz_id);
+  const quizz = await quizBelongToCourse(course_id, quiz_id);
   if (!quizz) throw new ApiError("quizz not found", 404);
 
   //check the options first
@@ -41,9 +44,36 @@ export const createQuestion = asyncHandler(async (req, rest) => {
     option_d,
     correct_option,
     question,
-    quize_id: quizz_id,
+    quiz_id,
   });
 
   if (!result) throw new ApiError("Internal server error", 500);
   rest.status(201).json({ message: "question created successfully" });
+});
+
+//**Delete a quizz question */
+export const deleteQuizzQuestion = asyncHandler(async (req, res) => {
+  const { course_id, quiz_id, question_id } = req.body;
+
+  //is course exist
+  const course = await getCourseById(course_id);
+  if (!course) throw new ApiError("course not exist", 404);
+
+  //check owner ship
+  const instructor = course.instructor_id.toString() === req.user.id.toString();
+  if (!instructor) throw new ApiError("Access denied", 403);
+
+  //is quizz belongs to course
+  const quizz = await quizBelongToCourse(course_id, quiz_id);
+  if (!quizz) throw new ApiError("quizz not exist", 404);
+
+  //is question belongs to quizz
+  const question = await questionBelongsToCourse(quiz_id, question_id);
+  if (!question) throw new ApiError("question not exist", 404);
+
+  //result
+  const result = deleteQuestion(question_id);
+  if (!result) throw new ApiError("Internal server error", 500);
+
+  res.status(204).json({ message: "message deleted successfully" });
 });
--- src/repository/questions.repository.js
+++ src/repository/questions.repository.js
@@ -3,9 +3,9 @@ import pool from "../config/db.config.js";
 //**create quesion for a quizz */
 export const createQuestion = async (data) => {
   const result = await pool.query(
-    "INSERT INTO questions (quize_id,question,option_a,option_b,option_c,option_d,correct_option)",
+    "INSERT INTO questions (quiz_id,question,option_a,option_b,option_c,option_d,correct_option)",
     [
-      data.quize_id,
+      data.quiz_id,
       data.question,
       data.option_a,
       data.option_b,
@@ -20,9 +20,9 @@ export const createQuestion = async (data) => {
 //**update quesion in a quize */
 export const updateQuestion = async (data, id) => {
   const result = await pool.query(
-    "UPDATE questions SET quize_id = ?,question = ?, option_a = ?, option_b = ?, option_c = ?, option_d  = ?, correct_option = ? WHERE id = ?",
+    "UPDATE questions SET quiz_id = ?,question = ?, option_a = ?, option_b = ?, option_c = ?, option_d  = ?, correct_option = ? WHERE id = ?",
     [
-      data.quize_id,
+      data.quiz_id,
       data.question,
       data.option_a,
       data.option_b,
@@ -41,16 +41,25 @@ export const deleteQuestion = async (id) => {
 };
 
 //**GET ALL QUESTION BELONGS TO A QUIZZ */
-export const quizzQuestions = async (quizz_id) => {
-  const [rows] = await pool.query(
-    "SELECT * FROM questions WHERE quizz_id = ?",
-    [quizz_id]
-  );
+export const quizzQuestions = async (quiz_id) => {
+  const [rows] = await pool.query("SELECT * FROM questions WHERE quiz_id = ?", [
+    quiz_id,
+  ]);
   return rows;
 };
 
 //**GET A question by Id */
 export const quesion = async (id) => {
-  const [rows] = await pool.query("SELECT * FROM question WHERE id = ? ", [id]);
+  const [rows] = await pool.query("SELECT * FROM questions WHERE id = ? ", [
+    id,
+  ]);
   return rows[0];
+};
+
+//**Get A quizz by quiz_id,and Id */
+export const questionBelongsToCourse = async (quiz_id, id) => {
+  const [rows] = await pool.query(
+    "SELECT * FROM question WHERE quiz_id = ? AND id = ?",
+    [quiz_id, id]
+  );
 };
