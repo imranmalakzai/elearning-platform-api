b860fef26c806c256794a75835b8e67e94777edf
imranmalakzai <afghanimran92@gmail.com>
2025-12-25 08:45:58 +0430

    create an update badge controller Admin only

--- src/controllers/badge.controller.js
+++ src/controllers/badge.controller.js
@@ -1,6 +1,10 @@
 import ApiError from "../utils/api_error.js";
 import asyncHandler from "../utils/async_handler.js";
-import { createBadge } from "../repository/badges.repository.js";
+import {
+  createBadge,
+  badgeById,
+  updateBadge as updateBadgeRepo,
+} from "../repository/badges.repository.js";
 
 //** create badges Admin only */
 export const createBadge = asyncHandler(async (req, res) => {
@@ -14,4 +18,26 @@ export const createBadge = asyncHandler(async (req, res) => {
   if (!badge) throw new ApiError("Internal server error", 500);
 
   res.status(201).json({ message: "badge created successfully" });
+});
+
+//**Updte bade Admin only */
+export const updateBadge = asyncHandler(async (req, res) => {
+  const { badge_id } = req.params;
+  const { name, description, points_required } = req.body;
+
+  if (!name || !description || !points_required) {
+    throw new ApiError("All fileds are required");
+  }
+
+  //badge exist
+  const badge = await badgeById(badge_id);
+  if (!badge) throw new ApiError("Badge not exist");
+
+  //badge
+  const result = await updateBadgeRepo(
+    { name, description, points_required },
+    badge_id
+  );
+  if (!result) throw new ApiError("Internal server error", 500);
+  res.status(200).json({ message: "Badge updated successfully" });
 });
