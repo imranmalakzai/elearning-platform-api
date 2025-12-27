c31fadd631224e88adc1e05e9d8e3a10c80282f9
imranmalakzai <afghanimran92@gmail.com>
2025-12-27 06:25:08 +0430

    renme the add userbadge repo

--- src/repository/user_badges.repository.js
+++ src/repository/user_badges.repository.js
@@ -1,10 +1,10 @@
 import pool from "../config/db.config.js";
 
 //**Give a badge to student */
-export const createStudentBadge = async (data) => {
+export const addUserBadge = async (user_id, badge_id) => {
   const result = await pool.query(
     "INSERT INTO user_badges (user_id,badge_id) VALUES (?,?)",
-    [data.user_id, data.badge_id]
+    [user_id, badge_id]
   );
   return result.affectedRows;
 };
