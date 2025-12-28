8bbb121e039ab665b73a0e8bdd700e01037153f8
imranmalakzai <afghanimran92@gmail.com>
2025-12-28 14:41:34 +0430

    create forum route end points

---
+++ src/routes/enrollment.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { auth } from "../middlewares/auth.middleware.js";
+import { allowRoles } from "../middlewares/allowed_roles.middleware.js";
+import {
+  cancellEnrollment,
+  courseStudents,
+  enrollToACourse,
+} from "../controllers/enrollment.controller.js";
+
+const enrollmentRouter = express.Router({ mergeParams: true });
+
+enrollmentRouter.use(auth);
+enrollmentRouter.route("/").post(enrollToACourse);
+enrollmentRouter.route("/:courseId").delete(cancellEnrollment);
+enrollmentRouter
+  .route("/:courseId")
+  .get(allowRoles("instructor"), courseStudents);
