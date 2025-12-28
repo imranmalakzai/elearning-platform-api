74faae576b7124454f5e977e2c78454657de7c52
imranmalakzai <afghanimran92@gmail.com>
2025-12-28 11:14:27 +0430

    create question route Api End points

---
+++ src/routes/question.route.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { auth } from "../middlewares/auth.middleware.js";
+import { allowRoles } from "../middlewares/allowed_roles.middleware.js";
+import {
+  createQuestion,
+  deleteQuizzQuestion,
+  updateQuizzQuestion,
+  getQuestion,
+  quizzQuestions,
+} from "../controllers/questions.controller.js";
+
+const questionRouter = express.Router();
+
+questionRouter.use(auth);
+
+//**Route End points */
+questionRouter.route("/").post(allowRoles("instructor"), createQuestion);
+questionRouter.route("/").get(quizzQuestions);
+questionRouter.route("/:questionId").get(getQuestion);
+
+questionRouter
+  .route("/:questionId")
+  .delete(allowRoles("instructor"), deleteQuizzQuestion);
+
+questionRouter
+  .route("/:questionId")
+  .put(allowRoles("instructor"), updateQuizzQuestion);
+
+export default questionRouter;
