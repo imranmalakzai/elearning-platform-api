a5dbc38c501bb24d70aafc0314c2e7ceb4383514
imranmalakzai <afghanimran92@gmail.com>
2025-12-28 09:15:31 +0430

    crete lesson route Api end points

---
+++ src/routes/lesson.route.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { auth } from "../middlewares/auth.middleware.js";
+import { allowRoles } from "../middlewares/allowed_roles.middleware.js";
+
+// controllers
+import {
+  courseLessons,
+  createLesson,
+  updateLesson,
+  deleteLesson,
+  getLesson,
+} from "../controllers/lessons.controller.js";
+
+const lessonRouter = express.Router({ mergeParams: true });
+
+/**
+ * ======================
+ * PUBLIC / ENROLLED USERS
+ * ======================
+ */
+lessonRouter.get("/", auth, courseLessons);
+lessonRouter.get("/:lessonId", auth, getLesson);
+
+/**
+ * ======================
+ * INSTRUCTOR ONLY
+ * ======================
+ */
+lessonRouter.post("/", auth, allowRoles("instructor"), createLesson);
+
+lessonRouter.put("/:lessonId", auth, allowRoles("instructor"), updateLesson);
+
+lessonRouter.delete(
+  "/:lessonId",
+  auth,
+  allowRoles("instructor", "admin"),
+  deleteLesson
+);
+
+export default lessonRouter;
+s;
