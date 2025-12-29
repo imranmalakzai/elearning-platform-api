921ae02e14ca77a604ae9e9270b06458073ee807
imranmalakzai <afghanimran92@gmail.com>
2025-12-29 06:23:48 +0430

    update course router end points and add neasted endpoints

--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -2,6 +2,16 @@ import express from "express";
 import { auth } from "../middlewares/auth.middleware.js";
 import { allowRoles } from "../middlewares/allowed_roles.middleware.js";
 
+// child routers
+import lessonRouter from "./lesson.route.js";
+import forumPostRouter from "./forum_post.route.js";
+import commentRouter from "./forum_comment.route.js";
+import quizRouter from "./quiz.route.js";
+import questionRouter from "./question.route.js";
+import quizAttemptRouter from "./quiz_atempt.route.js";
+import lessonProgressRouter from "./lesson_progress.route.js";
+import enrollmentRouter from "./enrollment.route.js";
+
 // controllers
 import {
   createCourseController,
@@ -19,22 +29,39 @@ const courseRouter = express.Router();
  * ======================
  */
 courseRouter.get("/", coursesController);
-courseRouter.get("/:id", getCourse);
+courseRouter.get("/:courseId", getCourse);
 
 /**
  * ======================
- * INSTRUCTOR ONLY
+ * INSTRUCTOR / ADMIN
  * ======================
  */
 courseRouter.post("/", auth, allowRoles("instructor"), createCourseController);
 
-courseRouter.put("/:id", auth, allowRoles("instructor"), updateCourse);
+courseRouter.put("/:courseId", auth, allowRoles("instructor"), updateCourse);
 
 courseRouter.delete(
-  "/:id",
+  "/:courseId",
   auth,
   allowRoles("instructor", "admin"),
   deleteCourseController
 );
+
+/**
+ * ======================
+ * NESTED RESOURCES
+ * ======================
+ */
+courseRouter.use("/:courseId/lessons", lessonRouter);
+
+courseRouter.use("/:courseId/forums", forumPostRouter);
+courseRouter.use("/:courseId/forums/:postId/comments", commentRouter);
+
+courseRouter.use("/:courseId/quizzes", quizRouter);
+courseRouter.use("/:courseId/quizzes/:quizId/questions", questionRouter);
+courseRouter.use("/:courseId/quizzes/:quizId/attempt", quizAttemptRouter);
+
+courseRouter.use("/:courseId/progress", lessonProgressRouter);
+courseRouter.use("/:courseId/enroll", enrollmentRouter);
 
 export default courseRouter;
