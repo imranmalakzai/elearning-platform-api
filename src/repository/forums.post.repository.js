c3f9b54d4effcc0d2a389b750046a2c5f906ebdc
imranmalakzai <afghanimran92@gmail.com>
2025-12-30 06:53:26 +0430

    fix the spelling essue in forum repository

--- src/repository/forums.post.repository.js
+++ src/repository/forums.post.repository.js
@@ -3,17 +3,17 @@ import pool from "../config/db.config.js";
 //**Create A post forum */
 export const createPost = async (data) => {
   const result = await pool.query(
-    "INSERT INTO forum_posts course_id,user_id,content VALUES (?,?,?)",
+    "INSERT INTO forum_posts (course_id,user_id,content) VALUES (?,?,?)",
     [data.course_id, data.user_id, data.content]
   );
   return result.affectedRows;
 };
 
 //**update a forum post */
-export const updatePost = async (content, id) => {
+export const updatePost = async (content) => {
   const result = await pool.query(
-    "UPDATE forum_posts SET , content = ? WHERE id = ?",
-    [content, id]
+    "UPDATE forum_posts SET content = ? WHERE id = ?",
+    [content, 1]
   );
   return result.affectedRows;
 };
