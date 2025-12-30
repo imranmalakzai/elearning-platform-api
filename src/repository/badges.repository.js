8a1925a7f62dcb6d666c072d45c9d85cc36326a6
imranmalakzai <afghanimran92@gmail.com>
2025-12-30 10:29:41 +0430

    fix badge repository file after testing

--- src/repository/badges.repository.js
+++ src/repository/badges.repository.js
@@ -3,7 +3,7 @@ import pool from "../config/db.config.js";
 //** create A badge */
 export const createBadge = async (data) => {
   const result = await pool.query(
-    "INSERT INTO badges (name,description,points_required)",
+    "INSERT INTO badges (name,description,points_required) VALUES (?,?,?)",
     [data.name, data.description, data.points_required]
   );
   return result.affectedRows;
