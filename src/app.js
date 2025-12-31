312ee248b455d63c523c714d27eaa2c201df3bc4
imranmalakzai <afghanimran92@gmail.com>
2025-12-31 12:11:31 +0430

    add swagger Api documentation file ionto the main app.js file

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ import courseRouter from "./routes/course.route.js";
 import badgeRouter from "./routes/badge.route.js";
 import leaderboardRouter from "./routes/leaderboard.route.js";
 
+//**Swagger setup */
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger.config.js";
+
 //error middleware
 import errorHandlerMiddleWare from "./middlewares/error_handler.middleware.js";
 
@@ -22,6 +26,7 @@ app.use("/api/", userRouter);
 app.use("/api/courses/", courseRouter);
 app.use("/api/badges/", badgeRouter);
 app.use("/api/leaderboards/", leaderboardRouter);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //**health end point */
 app.get("/", (req, res) => {
