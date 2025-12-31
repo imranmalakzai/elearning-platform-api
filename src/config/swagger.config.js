e02851da9974e381e59c25a15b2b104844e91d20
imranmalakzai <afghanimran92@gmail.com>
2025-12-31 12:11:09 +0430

    add swagger Api documentation config file

---
+++ src/config/swagger.config.js
@@ -0,0 +1,28 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+export const swaggerSpec = swaggerJSDoc({
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "E-learning management API",
+      version: "1.0.0",
+      description: "API documentation for e-learrning management system",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000/api",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+  apis: ["./src/routes/*.js"], // where comments are
+});
