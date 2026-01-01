a37da0d766baad3196e1a0554e1901e1abfd6436
imranmalakzai <afghanimran92@gmail.com>
2026-01-01 07:57:42 +0430

    create updateLessonSchema valiation input using zod

--- src/validation/lesson.chema.js
+++ src/validation/lesson.chema.js
@@ -16,3 +16,19 @@ export const createLessonSchema = z.object({
     .int("please provide number only")
     .maxValue(3, "please provide valid lesson order"),
 });
+
+export const updateLessonSchema = z.object({
+  title: z
+    .string()
+    .min(3, "lesson title must not be less then 3 characters")
+    .max(20, "lesson title must not be more then 20 characters"),
+  content: z
+    .string()
+    .min(15, "lesson content must not be less then 15 characters")
+    .max(20, "lesson content must not be more then 1000 characters"),
+  video_url: z.string(),
+  lesson_order: z
+    .number()
+    .int("please provide number only")
+    .maxValue(3, "please provide valid lesson order"),
+});
