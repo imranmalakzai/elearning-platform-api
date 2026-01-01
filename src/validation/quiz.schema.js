f59273c204f4834f1dc2eebaee2684e472fe1df2
imranmalakzai <afghanimran92@gmail.com>
2026-01-01 07:49:16 +0430

    create updateQuizz shema validation file

--- src/validation/quiz.schema.js
+++ src/validation/quiz.schema.js
@@ -5,3 +5,9 @@ export const createQuizSchema = z.object({
     .min(5, "quizz title should not be less then 5 characters")
     .max(50, "quizz title should not be more then 50 characters"),
 });
+
+export const updateQuizSchema = z.object({
+  title: string()
+    .min(5, "quizz title should not be less then 5 characters")
+    .max(50, "quizz title should not be more then 50 characters"),
+});
