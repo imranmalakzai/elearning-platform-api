0fd57a7c45f326a685ec9f411e12c58fae8582bd
imranmalakzai <afghanimran92@gmail.com>
2026-01-01 07:56:53 +0430

    create createLessonSchema validation file

---
+++ src/validation/lesson.chema.js
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+//  title, content, video_url, lesson_order
+export const createLessonSchema = z.object({
+  title: z
+    .string()
+    .min(3, "lesson title must not be less then 3 characters")
+    .max(20, "lesson title must not be more then 20 characters"),
+  content: z
+    .string()
+    .min(15, "lesson content must not be less then 15 characters")
+    .max(20, "lesson content must not be more then 1000 characters"),
+  video_url: z.string(),
+  lesson_order: z
+    .number()
+    .int("please provide number only")
+    .maxValue(3, "please provide valid lesson order"),
+});
