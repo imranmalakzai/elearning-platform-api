5a871811dbec2189ebaaefb4b2411ba0ec2130de
imranmalakzai <afghanimran92@gmail.com>
2026-01-01 18:46:21 +0430

    add validation middleare for forum_comment route endpoints

--- src/routes/forum_comment.route.js
+++ src/routes/forum_comment.route.js
@@ -1,5 +1,10 @@
 import express from "express";
 import { auth } from "../middlewares/auth.middleware.js";
+import { validate } from "../middlewares/validate.mddleware.js";
+import {
+  commentSchmea,
+  updateCommentSchma,
+} from "../validation/forum_comment.schema.js";
 import {
   postComment,
   editComment,
@@ -7,14 +12,17 @@ import {
   getCommentById,
   forumPostComments,
 } from "../controllers/forum_comments.controller.js";
+import { updateComment } from "../repository/forum_comments.repository.js";
 
 const commentRouter = express.Router({ mergeParams: true });
 commentRouter.use(auth);
 
-commentRouter.route("/").post(postComment);
+commentRouter.route("/").post(validate(commentSchmea), postComment);
 commentRouter.route("/").get(forumPostComments);
 commentRouter.route("/:commentId").get(getCommentById);
-commentRouter.route("/:commentId").patch(editComment);
+commentRouter
+  .route("/:commentId")
+  .patch(validate(updateCommentSchma), editComment);
 commentRouter.route("/:commentId").delete(deleteComment);
 
 export default commentRouter;
