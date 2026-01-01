4b5da011c06885d844f805c2328d80b41a0c81c4
imranmalakzai <afghanimran92@gmail.com>
2026-01-01 06:10:07 +0430

    create registration schema, use to validate the user input in registraion using Zod

---
+++ src/schema/users.schema.js
@@ -0,0 +1,13 @@
+import { email, z } from "zod";
+
+export const registerSchema = z.object({
+  name: z
+    .string()
+    .min(2, "name must be not be less then 3 characters")
+    .max(15, "name must not be more then 15 charcters "),
+  email: z.string().email("Invalid email address"),
+  password: z
+    .string()
+    .min(6, "password must be more then 6 characters")
+    .max(15, "password must not be more then 15 characters"),
+});
