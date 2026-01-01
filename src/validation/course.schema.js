66c11d61fc69782027d5e90b6c99fd57c4054376
imranmalakzai <afghanimran92@gmail.com>
2026-01-01 11:47:59 +0430

    optimize the zod error

--- src/validation/course.schema.js
+++ src/validation/course.schema.js
@@ -3,21 +3,21 @@ import { z } from "zod";
 export const createCourseShema = z.object({
   title: z
     .string()
-    .min(8, "title must be at least 8 characters")
-    .max(50, "title should not be more then 50 character"),
+    .min(8, "course title must be at least 8 characters")
+    .max(50, "course title should not be more then 50 character"),
   description: z
     .string()
-    .min(50, "description must be at least 50 characters")
+    .min(50, "course description must be at least 50 characters")
     .max(1000, "course description must not be more then 1000 characters"),
 });
 
 export const updateCourseShema = z.object({
   title: z
     .string()
-    .min(8, "title must be at least 8 characters")
+    .min(8, "course title must be at least 8 characters")
     .max(50, "title should not be more then 50 character"),
   description: z
     .string()
-    .min(50, "description must be at least 50 characters")
+    .min(50, "course description must be at least 50 characters")
     .max(1000, "course description must not be more then 1000 characters"),
 });
