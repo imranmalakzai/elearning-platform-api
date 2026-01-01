7fc2e63669e09469e97752e52fa0ad7d18ab6fb5
imranmalakzai <afghanimran92@gmail.com>
2026-01-01 07:41:52 +0430

    create post input valiation schema

---
+++ src/validation/forum_post.schema.js
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const createPostShema = z.object({
+  content: z
+    .string()
+    .min(10, "Post content should be at least 10 characters ")
+    .max(1000, "post content should not be more then 1000 characters"),
+});
