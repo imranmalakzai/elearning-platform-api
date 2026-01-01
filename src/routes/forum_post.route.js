96fba62338cd387bd4c08be31f75baf9dad5eef8
imranmalakzai <afghanimran92@gmail.com>
2026-01-01 16:55:43 +0430

    Add validation in forum post router endpoints

--- src/routes/forum_post.route.js
+++ src/routes/forum_post.route.js
@@ -7,15 +7,22 @@ import {
   deletePost,
   forumPosts,
 } from "../controllers/forum_posts.controller.js";
+import {
+  createPostShema,
+  updatePostShema,
+} from "../validation/forum_post.schema.js";
+import { validate } from "../middlewares/validate.mddleware.js";
 
 const forumPostRouter = express.Router({ mergeParams: true });
 
 forumPostRouter.use(auth);
 
-forumPostRouter.route("/").post(createForumPost);
+forumPostRouter.route("/").post(validate(createPostShema), createForumPost);
 forumPostRouter.route("/").get(forumPosts);
 forumPostRouter.route("/:postId").delete(deletePost);
-forumPostRouter.route("/:postId").patch(updateForumPost);
+forumPostRouter
+  .route("/:postId")
+  .patch(validate(updatePostShema), updateForumPost);
 forumPostRouter.route("/:postId").get(getPostById);
 
 export default forumPostRouter;
