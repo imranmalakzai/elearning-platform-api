bff4e91f2f542b974f608208bf1b3942cda6e992
imranmalakzai <afghanimran92@gmail.com>
2026-01-01 07:48:27 +0430

    create createQuizz schema file

---
+++ src/validation/quiz.schema.js
@@ -0,0 +1,7 @@
+import { string, z } from "zod";
+
+export const createQuizSchema = z.object({
+  title: string()
+    .min(5, "quizz title should not be less then 5 characters")
+    .max(50, "quizz title should not be more then 50 characters"),
+});
