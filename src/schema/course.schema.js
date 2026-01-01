02bacb8b7b86668ead58d6854f0469ea5b2629bd
imranmalakzai <afghanimran92@gmail.com>
2026-01-01 06:48:57 +0430

    create updateCourseShema input validator using zod

--- src/schema/course.schema.js
+++ src/schema/course.schema.js
@@ -10,3 +10,14 @@ export const createCourseShema = z.object({
     .min(50, "description must be at least 50 characters")
     .max(1000, "course description must not be more then 1000 characters"),
 });
+
+export const updateCourseShema = z.object({
+  title: z
+    .string()
+    .min(8, "title must be at least 8 characters")
+    .max(50, "title should not be more then 50 character"),
+  description: z
+    .string()
+    .min(50, "description must be at least 50 characters")
+    .max(1000, "course description must not be more then 1000 characters"),
+});
