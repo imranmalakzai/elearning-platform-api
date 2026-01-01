13e2bdcd9b885c7825f6081e6c4e0b8dfbba4872
imranmalakzai <afghanimran92@gmail.com>
2026-01-01 07:33:54 +0430

    create update badge schema input validation using zod

--- src/validation/badge.schema.js
+++ src/validation/badge.schema.js
@@ -15,3 +15,19 @@ export const createBadgeSchema = z.object({
     .gte(10, "required points should be more then 10")
     .lte(5000, "required points should not be more then 5000"),
 });
+
+export const updateBadgeSchema = z.object({
+  name: z
+    .string()
+    .min(3, "badge name should be at leaet 3 character")
+    .max(10, "badge name should not be more then 10 characters"),
+  description: z
+    .string()
+    .min(5, "badge description should be at least 5 characters")
+    .max(25, "badge description should not be more then 25 characters"),
+  points_required: z
+    .number("please provide number")
+    .int("please provide positive number")
+    .gte(10, "required points should be more then 10")
+    .lte(5000, "required points should not be more then 5000"),
+});
