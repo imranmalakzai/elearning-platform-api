37ced5e1884730835a62c5d322c723517986d4c5
imranmalakzai <afghanimran92@gmail.com>
2026-01-01 20:29:06 +0430

    create quiz attemp route end points

--- src/routes/quiz_atempt.route.js
+++ src/routes/quiz_atempt.route.js
@@ -1,10 +1,12 @@
 import e from "express";
 import { auth } from "../middlewares/auth.middleware.js";
 import { attemptQuiz } from "../controllers/quiz_attempts.controller.js";
+import { validate } from "../middlewares/validate.mddleware.js";
+import { quizAttemptSchema } from "../validation/quiz_attempt.schema.js";
 
 const quizAttemptRouter = e.Router({ mergeParams: true });
 quizAttemptRouter.use(auth);
 
-quizAttemptRouter.route("/").post(attemptQuiz);
+quizAttemptRouter.route("/").post(validate(quizAttemptSchema), attemptQuiz);
 
 export default quizAttemptRouter;
