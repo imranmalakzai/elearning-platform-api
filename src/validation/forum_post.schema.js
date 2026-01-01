4dbce8851c1d5748a130d03d14ea1563ce4ee6e6
imranmalakzai <afghanimran92@gmail.com>
2026-01-01 07:43:04 +0430

    create update user schema valiation file

--- src/validation/forum_post.schema.js
+++ src/validation/forum_post.schema.js
@@ -6,3 +6,10 @@ export const createPostShema = z.object({
     .min(10, "Post content should be at least 10 characters ")
     .max(1000, "post content should not be more then 1000 characters"),
 });
+
+export const updatePostShema = z.object({
+  content: z
+    .string()
+    .min(10, "Post content should be at least 10 characters ")
+    .max(1000, "post content should not be more then 1000 characters"),
+});
