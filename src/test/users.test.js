8d0f2ecbc4e5b667def56e45f0c25da75472ce4e
imranmalakzai <afghanimran92@gmail.com>
2026-01-03 06:27:42 +0430

    add automate testing for registration schem check validation using invalid email address

--- src/test/users.test.js
+++ src/test/users.test.js
@@ -9,8 +9,15 @@ describe("AUTH API", () => {
       email: "admin@gmail.com",
       password: "admin@123",
     });
-
     expect(res.statusCode).toBe(200);
     expect(res.body.token).toBeDefined();
+  });
+  test("Register user with invalid email address should return statusCode of 400", async () => {
+    const res = await request(app).post("/api/auth/register").send({
+      name: "haji",
+      email: "kfjkasdjfalksd",
+      password: "super_secret_key",
+    });
+    expect(res.statusCode).toBe(400);
   });
 });
