a6458a34c6dca34914d5ff96eabab0bcdf67cf39
imranmalakzai <afghanimran92@gmail.com>
2026-01-27 17:04:30 +0430

    fix(env): fix undefine issue with in env.config.js file

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-dotenv.config({ path: `.env.${"production" || "development"}` });
+dotenv.config({ path: `.env.${"development" || "production"}` });
 
 export const { PORT, HOST, DB_NAME, DB_USER, JWT_SECRET, DB_PASSWORD } =
   process.env;
