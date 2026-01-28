7feeb4508f88da577ac0bd44ecfe2f1c726e7b93
imranmalakzai <afghanimran92@gmail.com>
2026-01-28 09:52:33 +0430

    fix : correct redis clent configuration

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -1,6 +1,16 @@
 import { createClient } from "redis";
 
-const redis = createClient();
-redis.on("error", (err) => console.log("Redis error", err));
+const redis = createClient({
+  url: "redis://127.0.0.1:6379",
+});
+
+redis.on("connect", () => {
+  console.log("Redis conected successfully");
+});
+
+redis.on("error", (err) => {
+  console.log("Redis Error");
+});
+
 await redis.connect();
 export default redis;
