cb8dfcdd64a6efcaf425c7a312e28fb31529a095
imranmalakzai <afghanimran92@gmail.com>
2026-01-28 16:30:17 +0430

    fix : correct authentication middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,14 +3,18 @@ import { JWT_SECRET } from "../config/env.config.js";
 import ApiError from "../utils/api_error.js";
 
 export const auth = async (req, res, next) => {
-  const header = req.headers.authorization;
-  if (!header || !header.startsWith("Bearer")) {
-    throw new ApiError("please authenticate", 401);
+  try {
+    const header = req.headers.authorization;
+    if (!header || !header.startsWith("Bearer")) {
+      throw new ApiError("please authenticate", 401);
+    }
+    const token = header.split(" ")[1];
+    const decode = jwt.verify(token, JWT_SECRET);
+    if (!decode) throw new ApiError("Invalid token", 401);
+    req.token = token;
+    req.user = decode;
+    next();
+  } catch (error) {
+    throw new ApiError("Invalid token or token is expired", 401);
   }
-  const token = header.split(" ")[1];
-  const decode = jwt.verify(token, JWT_SECRET);
-  if (!decode) throw new ApiError("Invalid token", 401);
-  req.token = token;
-  req.user = decode;
-  next();
 };
